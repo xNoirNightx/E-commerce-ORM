18bc165d029e2e5f875e5a0134b7a2c6ae324bfb
Coral <cnight18@hotmail.com>
2023-09-02 09:39:56 -0300

    datagrip tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,32 @@
--- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+-- following README install , add this code to your workbench / data grip 
+CREATE DATABASE IF NOT EXISTS ecommerce_db;
+USE ecommerce_db;
 
--- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE TABLE IF NOT EXISTS category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS product (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE IF NOT EXISTS tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS product_tag (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES product(id),
+  FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
+
+SHOW TABLES;
